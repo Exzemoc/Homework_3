00777cb8b364ed54376304e12079035e9dfa2114
Vlad <120489891+Exzemoc@users.noreply.github.com>
2023-01-10 20:21:31 +0300

    Домашка

---
+++ 4 Задания/2.py
@@ -0,0 +1,16 @@
+age = input('Ваш возраст: ')
+name = str(input('Ваше имя: '))
+
+for i in age:
+    if int(age) > 100:
+        print(name.capitalize(), "Вы лжёте, в наше время столько не живут... ")
+        break
+    elif 0 <= int(age) <= 10:
+        print("Привет шкет,", name.capitalize())
+        break
+    elif 10 < int(age) <= 18:
+        print("Как жизнь", name.capitalize(), "?")
+        break
+    elif 18 < int(age) <= 100:
+        print("Что пожелаете", name.capitalize(), '?')
+        break
---
+++ 4 Задания/4.py
@@ -0,0 +1,3 @@
+x = int(input('Число: '))
+y = sum(i ** 3 for i in range(1, x + 1))
+print(y)
