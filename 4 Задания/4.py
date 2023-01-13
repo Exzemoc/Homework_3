d95e8a2184132d0977fbfd23f6145500155f4d66
Vlad <120489891+Exzemoc@users.noreply.github.com>
2023-01-13 19:02:31 +0300

    update

--- 4 Задания/4.py
+++ 4 Задания/4.py
@@ -2,10 +2,10 @@ x = int(input('Число: '))
 y = sum(i ** 3 for i in range(1, x + 1))
 print(y)
 
-n = int(input())
+n = int(input('Число: '))
 c = 0
 while 1 <= n:
     n -= 1
     b = (n + 1) ** 3
     c += b
-print(c, 'сумма кубов')
+print(c)
