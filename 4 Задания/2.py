36bc564ddc0685353360d393f35e1a728421f7c9
Vlad <120489891+Exzemoc@users.noreply.github.com>
2023-01-13 15:33:47 +0300

    update

--- 4 Задания/2.py
+++ 4 Задания/2.py
@@ -3,14 +3,14 @@ name = str(input('Ваше имя: '))
 
 for i in age:
     if int(age) > 100:
-        print(name.capitalize(), "Вы лжёте, в наше время столько не живут... ")
+        print(name.title(), "Вы лжёте, в наше время столько не живут... ")
         break
     elif 0 <= int(age) <= 10:
-        print("Привет шкет,", name.capitalize())
+        print("Привет шкет,", name.title())
         break
     elif 10 < int(age) <= 18:
-        print("Как жизнь", name.capitalize(), "?")
+        print("Как жизнь", name.title(), "?")
         break
     elif 18 < int(age) <= 100:
-        print("Что пожелаете", name.capitalize(), '?')
+        print("Что пожелаете", name.title(), '?')
         break
--- 4 Задания/3.py
+++ 4 Задания/3.py
@@ -4,16 +4,16 @@ while True:
 
     for i in age:
         if int(age) > 100:
-            print(name.capitalize(), "Вы лжёте, в наше время столько не живут... ")
+            print(name.title(), "Вы лжёте, в наше время столько не живут... ")
             break
         elif 0 <= int(age) <= 10:
-            print("Привет шкет,", name.capitalize())
+            print("Привет шкет,", name.title())
             break
         elif 10 < int(age) <= 18:
-            print("Как жизнь", name.capitalize(), "?")
+            print("Как жизнь", name.title(), "?")
             break
         elif 18 < int(age) <= 100:
-            print("Что пожелаете", name.capitalize(), '?')
+            print("Что пожелаете", name.title(), '?')
             break
     cont = input(' Хотите продолжить? Напишите Y/N:')
     if cont == "n" or cont == "N":
